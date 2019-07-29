a1fc9a228c302fb98caad334b3321810e895e94b
Adam Modzelewski <adam.modzelewski@starlingbank.com>
2019-07-29 15:53:26 +0100

    Add Swift Package Manager manifest file

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "RxSwiftExt",
+    platforms: [
+      .macOS(.v10_11), .iOS(.v8), .tvOS(.v9)
+    ],
+    products: [
+        .library(name: "RxSwiftExt", targets: ["RxSwiftExt"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "RxSwiftExt",
+            dependencies: ["RxSwift", "RxCocoa"],
+            path: "Source"
+        ),
+        .testTarget(
+            name: "RxSwiftExt", 
+            dependencies: ["RxSwiftExt", "RxTest"],
+            path: "Tests"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
